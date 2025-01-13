d342613a5b92330394d8fa85266687031c2b7648
Liam Esseen <Liam.esseen@elev.ga.ntig.se>
2025-01-13 09:24:50 +0100

    Andra commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        Kvadrat(5);
+        int Hello32 = 0;
 
+        while (Hello32 < 32) {
+            System.out.println("Hello world");
+            Hello32++;
+        }
+    }
+
+        public static void Kvadrat(int tal) {
+
+        int kvadrat = tal * tal;
+
+            System.out.println("Kvadraten av "+ tal +"är" + kvadrat);
+
+        }
+    }
